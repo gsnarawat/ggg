69d901abbb2daac22ed39e3d025163b111c791df
rathore <gopalnarawat@gmail.com>
2013-10-03 06:34:02 +0530

    just check

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ganeshaynamah::Application.config.secret_token = '982f70349d8b68882c4d52239244cb8a09398885b292b7256c159c94fd86baf00b5d61a37af0f8972494b2a3c67c25e8203c5f6cce30f22936ecfb6276a6c910'
